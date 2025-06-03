31598c0364261b02c61332328753457f9bb12d60
treslecheson <tr3ysonb@gmail.com>
2025-06-02 23:21:05 -0600

    works one time through(kind of)

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 const alive string = "X"
 const dead string = "0"
 
 type Board [][]string
+
+func board() Board {
+	row1 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+	row2 := []string{dead, dead, alive, alive, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+	row3 := []string{dead, alive, alive, alive, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+	row4 := []string{dead, dead, alive, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+	row5 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+	row6 := []string{dead, dead, dead, dead, dead, dead, dead, dead, alive, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+	row7 := []string{dead, dead, dead, dead, dead, dead, dead, dead, alive, alive, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+	row8 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+	row9 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+	row10 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+
+	return [][]string{row1, row2, row3, row4, row5, row6, row7, row8, row9, row10}
+}
+
+func printBoard(board [][]string) {
+	for i := 0; i < len(board); i++ {
+		fmt.Println(board[i])
+	}
+}
 
 func contains(row []string, target string) int {
 	count := 0
@@ -30,7 +50,7 @@ func indexes(row []string) []int {
 	return indicesList
 }
 
-func spaceAround(checking int, above, below []string) bool {
+func neighbors(checking, cellLow, cellHigh int, above, current, below []string) bool {
 	cellsAround := 0
 	aboveIdices := indexes(above)
 	belowIdices := indexes(below)
@@ -54,53 +74,63 @@ func spaceAround(checking int, above, below []string) bool {
 			cellsAround++
 		}
 	}
-	if cellsAround == 2 {
-		return true
-	} else if cellsAround == 3 {
+
+	if current[checking-1] == alive {
+		cellsAround++
+	} else if current[checking+1] == alive {
+		cellsAround++
+	}
+
+	fmt.Println(cellsAround)
+	if cellLow <= cellsAround && cellsAround <= cellHigh {
 		return true
 	}
 	return false
 }
 
-func board() Board {
-	row1 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
-	row2 := []string{dead, alive, alive, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
-	row3 := []string{dead, dead, alive, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
-	row4 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
-	row5 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
-	row6 := []string{dead, dead, dead, dead, dead, dead, dead, dead, alive, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
-	row7 := []string{dead, dead, dead, dead, dead, dead, dead, dead, alive, alive, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
-	row8 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
-	row9 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
-	row10 := []string{dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead, dead}
+func solitude(above, below []string) bool {
 
-	return [][]string{row1, row2, row3, row4, row5, row6, row7, row8, row9, row10}
+	if contains(above, alive) == 0 && contains(below, alive) == 0 {
+		return true
+	}
+	return false
 }
 
-func solitude(board Board) Board {
-	for i := 0; i < len(board); i++ {
-		if contains(board[i], alive) == 1 {
-			if contains(board[i-1], alive) == 0 && contains(board[i+1], alive) == 0 {
-				fmt.Println("Row " + strconv.Itoa(i) + " died")
-			}
-		}
+func populate(checking, cellLow, cellHigh int, above, current, below []string) {
+	if neighbors(checking, cellLow, cellHigh, above, current, below) == true {
+		current[checking] = alive
 	}
-
-	return board
-
 }
 
-func printBoard(board [][]string) {
-	for i := 0; i < len(board); i++ {
-		fmt.Println(board[i])
+func kill(checking, cellLow, cellHigh int, above, current, below []string) {
+	if neighbors(checking, cellLow, cellHigh, above, current, below) == true {
+		current[checking] = dead
 	}
-
 }
 
 func main() {
 	printBoard(board())
-	solitude(board())
 	board := board()
 
-	fmt.Println(spaceAround(2, board[1], board[3]))
+	for i := 0; i < 100; i++ {
+		for i := 2; i < len(board)-2; i++ {
+			for x := 2; x < len(board[i])-3; x++ {
+				if board[i][x] == alive {
+					kill(x, 4, 8, board[i-1], board[i], board[i+1])
+				} else {
+					populate(x, 3, 3, board[i-1], board[i], board[i+1])
+					fmt.Println("  ")
+					fmt.Println("  ")
+					fmt.Println("  ")
+					fmt.Println("------------------------------------------------------")
+				}
+				printBoard(board)
+			}
+
+		}
+
+	}
+
+	printBoard(board)
+
 }
